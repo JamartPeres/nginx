f756770c3db1a1e0ab90977b891be221eee733f9
Jamart <86027854+JamartPeres@users.noreply.github.com>
2024-02-28 18:46:30 -0600

    Add files via upload

---
+++ deploy/deploy.sh
@@ -0,0 +1,15 @@
+systemctl stop nginx
+
+cd nginx
+git pull
+mv -v * /var/www/html
+
+systemctl start nginx
+
+ngrok config add-authtoken 2cygh7xTCpVe7vg9HIe0xpbQtuE_41YFRLCAMrNRwe1Ft8MFh
+ngrok http http://192.168.100.151:80 > /dev/null &
+
+#Para consultar la url
+ngrok_url=$(curl -s http://localhost:4040/api/tunnels | jq -r .tunnels[0].public_url)
+
+echo "URL del sitio: $ngrok_url"
